0a70f0956023538aad71f00cc2cd003ad1b568d1
Serenasaguier <ssaguier@udesa.edu.ar>
2022-06-02 15:32:39 -0300

    Borrando html y js deberia funcionar pero la api no me deja

---
+++ javascript/index.js
@@ -0,0 +1,68 @@
+ 
+ /*ALBUMES 
+
+ let albumes = [ ' '];
+ let url = 'https://thingproxy.freeboard.io/fetch/https://developers.deezer.com/api/album ';
+ fetch(url)
+ .then(function (response) {
+   return response.json();
+ }) 
+ .then(function (data) {
+   console.log(data);
+   albumes = data;
+
+let cantan = document.querySelector('.section2')
+let cantantes = [ ' '];
+
+for (let i = 0; i < 6; i++) {
+  cantantes.innerHTML += `<article class="article2">
+  <a href="detail-album.html" class="sacardilineado">
+    <div class="contenedorfoto">  <img src="${data[i].cover}" alt=""></div>
+        <h3> ${data[i].title}</h3>
+        <h4>${data[i].artist.name}</h4>
+        <p>${data[i].release_date}</p>
+    </a>
+</article>`;
+  
+}
+
+cantan.innerHTML = cantantes
+
+ })
+ .catch(function (error) {
+   console.log('El error fue:' + error)
+ })
+
+ 
+ */
+ 
+ /* ARTISTAS */
+
+ 
+ let artistas = ['  '];
+
+fetch('https://thingproxy.freeboard.io/fetch/' + 'https://developers.deezer.com/api/artist ' )
+.then(function (response) {
+    return response.json();
+    
+})
+.then(function (data) {
+  console.log(data)
+  artistas = data;
+    
+ let caract = document.querySelector(".section3")
+
+for (let i = 0; i < 6; i++) {
+  
+  caract.innerHTML += `<article>
+  <img src="${artistas[i].picture}" alt='' />
+   <p>Name: ${artistas[i].name} </p>
+</article>
+<hr>
+`;
+}
+
+})
+.catch(function (error) {
+    console.log('El error fue' + error)
+}) 
