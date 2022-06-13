303669a9a65c51356bae488511e30a2c25610a5b
Serenasaguier <ssaguier@udesa.edu.ar>
2022-06-13 16:35:01 -0300

    cosas terminadas

--- javascript/detail-artist.js
+++ javascript/detail-artist.js
@@ -2,33 +2,59 @@ let query = location.search;
 let queryString = new URLSearchParams(query);
 let agarrandolo = queryString.get('id');
 
-const url = `https://api.allorigins.win/raw?url=https://api.deezer.com/chart/artist?id= ${agarrandolo}`;
+const url = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${agarrandolo}`;
 
 fetch(url)
 .then(function (response) {
     return response.json();
 })
-.then(function (data) {
-    let info = data.tracks.data;
-  
-    console.log(info);
+.then(function (artista) { 
+    let artistaEtiqueta = document.querySelector('#detalleArtista');
+
+    artistaEtiqueta.innerHTML += ` <article class="article1 detail-artist">
+                            <a href="detail-track.html" class="sacardilineado">
+                            <div class="contenedorfoto"> <img src="${artista.picture}" alt=""></div>
+                            <h3>Name : ${artista.name}</h3>
+                             
+                        </a>
+                        </article>`
+
     
-    let artista = document.querySelector('.section1');
-    artista.innerHTML += ` <article class="article1 detail-artist">
-    <a href="detail-track.html" class="sacardilineado">
-   <div class="contenedorfoto"> <img src="${info.artist.picture}" alt=""></div>
-    <h3>Name : ${info.artist.name}</h3>
-    <h4> <ul>
-        <li> ${info.tracklist}</li>
-        <li>Bad Guy</li>
-        <li> I love you</li>
-        <li> Ocena eyes</li>
-        <li> Bored</li>
-    </ul> </h4>
-</a>
-</article>`
 })
 
 .catch(function (error) {
     console.log('Error:'+ error);
 })
+
+let linkTracks = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/tracklist`
+fetch(linkTracks)
+.then(function (response) {
+    response.json();
+})
+.then(function (data) {
+    console.log(data)
+    let ulArtista = document.querySelector('.listaTracks')
+    for (let i = 0; i <= 5; i++) {
+       ulArtista.innerHTML=`li> ${data[i].title}</li>` ;
+        
+    }
+})
+
+/* Respuesta de busquedas -- funciona */
+
+
+let form = document.querySelector('form'); 
+let busqueda = document.querySelector('.buscar');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (busqueda.value == "" || busqueda.value.length <= 2) {
+        let vacio = document.querySelector('main');
+        vacio.innerHTML = `<h3 > Esta vacio el campo o debe ser mayor a 2 caracteres </h3> ` ;
+        vacio.style.margin = "270px" 
+    } else {
+        this.submit();
+    }
+
+}) 
--- javascript/index.js
+++ javascript/index.js
@@ -10,7 +10,7 @@ fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks'
   console.log(info);
   let songs = document.querySelector(".section1")
   for (let i = 0; i < 6; i++) {
-    songs.innerHTML += ` <article  class="article1">
+    songs.innerHTML += `<article  class="article1">
     <a href="detail-track.html?id=${info[i].id}" class="sacardilineado">
         <div class="contenedorfoto"><img src="${info[i].album.cover}" alt=""> </div>
         <h3 class="canciones">${ info[i].title_short}</h3>
@@ -40,14 +40,13 @@ let cantan = document.querySelector('.section2')
 
 for (let i = 0; i < 6; i++) {
  cantan.innerHTML += `<article class="article2">
- <a href="detail-album.html?id=${info.data[i].id}">
+ <a href="detail-album.html?id=${info.data[i].id}" class="sacardilineado">
    <div class="contenedorfoto">  <img src="${info.data[i].cover}" alt=""></div>
        <h3> ${info.data[i].title}</h3>
        <h4>${info.data[i].artist.name}</h4>
        </a>
    
-</article>`;
- 
+</article>`; 
 }
 
 
@@ -73,7 +72,7 @@ fetch( `https://api.allorigins.win/raw?url=https://api.deezer.com/chart/artist` 
   for (let i = 0; i < 6; i++) {
      
   caract.innerHTML += `<article class="article3">
-  <a href="detail-artist.html?id= ${info[i].artist.id}" class="sacardilineado">
+  <a href="detail-artist.html?id=${info[i].artist.id}" class="sacardilineado">
   <div class="contenedorfoto"> <img src="${info[i].artist.picture}" alt=""> </div>
     <h3>${info[i].artist.name}</h3>
       <h4>${info[i].album.title}</h4>
@@ -82,14 +81,13 @@ fetch( `https://api.allorigins.win/raw?url=https://api.deezer.com/chart/artist` 
 </article>
 `; 
     
-  } /*     
-     */
+  } 
 
 }).catch(function(error) {
   console.log(error);
 })
  
-/* Respuesta de busquedas -- No funciona */
+/* Respuesta de busquedas -- funciona */
 
 
 let form = document.querySelector('form'); 
@@ -99,10 +97,9 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     if (busqueda.value == "" || busqueda.value.length <= 2) {
-        let vacio = document.querySelector('.section1');
-        vacio.innerHTML = `<h3 > Esta vacio el campo o debe ser mayor a 2 caracteres </h3> ` ; 
-        let sacar = document.querySelector('.section2 ');
-        sacar.style.display = "none";
+        let vacio = document.querySelector('main');
+        vacio.innerHTML = `<h3 > Esta vacio el campo o debe ser mayor a 2 caracteres </h3> ` ;
+        vacio.style.margin = "270px" 
     } else {
         this.submit();
     }
