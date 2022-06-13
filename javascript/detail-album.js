8508436c4480cc77227a7872fe15594502d9305f
Serenasaguier <ssaguier@udesa.edu.ar>
2022-06-13 16:37:46 -0300

    album terminado

--- javascript/detail-album.js
+++ javascript/detail-album.js
@@ -1,18 +1,18 @@
 
-/* NO FUNCIONA */
+/* FUNCIONA */
 
 let queryS = location.search;
 let queryString = new URLSearchParams(queryS);
 let agarrando = queryString.get('id');
 
-const link = `https://api.allorigins.win/raw?url=https://api.deezer.com/chart/albums?id=${agarrando}` ;
+const link = `https://api.allorigins.win/raw?url=https://api.deezer.com/album/${agarrando}` ;
 
 fetch(link)
 .then(function (response) {
     return response.json();
 })
 .then(function (data) {
-    let info = data.tracks.data;
+    let info = data;
  
  console.log(info);
 let albums = document.querySelector('.section1')
@@ -21,20 +21,53 @@ albums.innerHTML += `  <article class="largo" >
             
 <div class="contenedorfoto"><img src=" ${info.cover}" alt=""> </div> 
 <ul >
-<li>${info.title}</li>
-<a href="./detail-artist.html" class="sacardilineado largo" ><li>${info.artist}</li> </a>
- <a href="./detail-genres.html" class="sacardilineado largo"> <li></li></a>
-<li></li>
-</ul>
+<li>Album : ${info.title}</li>
+<a href="./detail-artist.html" class="sacardilineado largo" ><li> Arista : ${info.artist.name}</li> </a>
+</ul 
 <h3>Canciones</h3>
 <a href="./detail-track.html" class="sacardilineado largo"> 
-   <ul>
-    <li>${info.tracklist}</li>
-    <li>Go Flex</li>
-    <li>Congratulations</li>
-    <li>I Fall Apart</li>
-</ul> 
+    <ul id="cancionesAlbum">
+     
+ </ul> 
 </a>
-</article> `
+</article> </article>`
+
+}) 
+.catch(function (error) {
+    console.log("Error" + error)
+})
+
+/* Las canciones */
+fetch(link)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    let info = data;
+ console.log(info);
+
+ for (let i = 0; i <= 4; i++) {
+     document.querySelector('#cancionesAlbum').innerHTML += `<li>${data.tracks.data[i].title_short}</li> `;
+ }
+   
+ })
+
+
+/* Respuesta de busquedas -- funciona */
+
+
+let form = document.querySelector('form'); 
+let busqueda = document.querySelector('.buscar');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (busqueda.value == "" || busqueda.value.length <= 2) {
+        let vacio = document.querySelector('main');
+        vacio.innerHTML = `<h3 > Esta vacio el campo o debe ser mayor a 2 caracteres </h3> ` ;
+        vacio.style.margin = "270px" 
+    } else {
+        this.submit();
+    }
 
 }) 
